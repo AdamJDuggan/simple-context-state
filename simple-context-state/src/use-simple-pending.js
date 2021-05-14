455681ce7185ed12033c4c0824a30bbc856aae22
AdamJDuggan <adamduggan17@gmail.com>
2021-05-14 12:14:14 +0100

    tidy up to usePendingHook

--- simple-context-state/src/use-simple-pending.js
+++ simple-context-state/src/use-simple-pending.js
@@ -5,22 +5,25 @@ const useSimplePending = (...actions) => {
   const { pending } = useContext(GlobalContext);
   const matchedPending = [];
   if (!actions.length) {
-    pending.map((error) => matchedPending.push(error));
+    pending.map((pendingAction) => matchedPending.push(pendingAction));
   } else {
     actions.map((action) => {
       if (!action.split("_")[1]) {
-        pending.find((error) => {
+        pending.find((pendingAction) => {
           if (
-            error.split("_")[0] === action.split("_")[0] &&
-            !matchedPending.includes(error)
+            pendingAction.split("_")[0] === action.split("_")[0] &&
+            !matchedPending.includes(pendingAction)
           ) {
-            matchedPending.push(error);
+            matchedPending.push(pendingAction);
           }
         });
       } else {
-        pending.map((error) => {
-          if (error === action && !matchedPending.includes(error))
-            matchedPending.push(error);
+        pending.map((pendingAction) => {
+          if (
+            pendingAction === action &&
+            !matchedPending.includes(pendingAction)
+          )
+            matchedPending.push(pendingAction);
         });
       }
     });
